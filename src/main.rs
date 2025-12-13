bf89f1e5bead7ae322fc8d9ff5744814511724ab
Leonardo Villalobos <villaleobos@gmail.com>
2025-12-13 01:37:17 -0800

    add tests

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use crate::util::process_input;
-use std::io::{self, Write};
+use std::io::{self, BufRead, Write};
 
 mod util;
 
@@ -7,13 +7,12 @@ fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
 
-    let _ = find_command(&read_command());
+    let stdin = io::stdin().lock();
+    let _ = find_command(&read_command(stdin));
 }
 
-fn read_command() -> String {
-    let stdin = io::stdin().lock();
-
-    match process_input(stdin) {
+fn read_command<R: BufRead>(reader: R) -> String {
+    match process_input(reader) {
         Ok(input) => input,
         Err(_) => unreachable!(),
     }
@@ -23,4 +22,27 @@ fn find_command(name: &str) -> Option<()> {
     // All commands are unknown for now
     println!("{name}: command not found");
     None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_read_command() {
+        let input = "go";
+        let reader = io::Cursor::new(input);
+        let cmd_name = read_command(reader);
+
+        assert_eq!(input, cmd_name);
+    }
+
+    #[test]
+    fn should_find_command() {
+        let cmd_name = "go";
+        let cmd = find_command(cmd_name);
+
+        // All commands are unknown for now
+        assert!(cmd.is_none());
+    }
 }
