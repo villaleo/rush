bf79adef735183364ec12b18fa03561279a93a11
Leonardo Villalobos <villaleobos@gmail.com>
2025-12-13 02:34:39 -0800

    move logic into util.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use crate::util::process_input;
-use std::io::{self, BufRead, Write};
+use crate::util::{find_command, read_command};
+use std::io::{self, Write};
 
 mod util;
 
@@ -10,41 +10,5 @@ fn main() {
 
         let stdin = io::stdin().lock();
         let _ = find_command(&read_command(stdin));
-    }
-}
-
-fn read_command<R: BufRead>(reader: R) -> String {
-    match process_input(reader) {
-        Ok(input) => input,
-        Err(_) => unreachable!(),
-    }
-}
-
-fn find_command(name: &str) -> Option<()> {
-    // All commands are unknown for now
-    println!("{name}: command not found");
-    None
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn should_read_command() {
-        let input = "go";
-        let reader = io::Cursor::new(input);
-        let cmd_name = read_command(reader);
-
-        assert_eq!(input, cmd_name);
-    }
-
-    #[test]
-    fn should_find_command() {
-        let cmd_name = "go";
-        let cmd = find_command(cmd_name);
-
-        // All commands are unknown for now
-        assert!(cmd.is_none());
     }
 }
--- src/util.rs
+++ src/util.rs
@@ -6,3 +6,39 @@ pub fn process_input<R: BufRead>(mut reader: R) -> io::Result<String> {
 
     Ok(input.trim().into())
 }
+
+pub fn read_command<R: BufRead>(reader: R) -> String {
+    match process_input(reader) {
+        Ok(input) => input,
+        Err(_) => unreachable!(),
+    }
+}
+
+pub fn find_command(name: &str) -> Option<()> {
+    // All commands are unknown for now
+    println!("{name}: command not found");
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_read_command() {
+        let input = "go";
+        let reader = io::Cursor::new(input);
+        let cmd_name = read_command(reader);
+
+        assert_eq!(input, cmd_name);
+    }
+
+    #[test]
+    fn should_find_command() {
+        let cmd_name = "go";
+        let cmd = find_command(cmd_name);
+
+        // All commands are unknown for now
+        assert!(cmd.is_none());
+    }
+}
