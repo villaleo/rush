ad4ded32ccf2b43c0e59fb4de230a181dc3dafef
Leonardo Villalobos <villaleobos@gmail.com>
2025-12-13 11:46:48 -0800

    move logic into impl for Command

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use crate::util::read_command;
+use crate::util::Command;
 use std::io::{self, Write};
 
 mod util;
@@ -9,7 +9,7 @@ fn main() {
         io::stdout().flush().unwrap();
 
         let stdin = io::stdin().lock();
-        let cmd = read_command(stdin).unwrap();
+        let cmd = Command::new(stdin).unwrap();
         match cmd.type_ {
             util::CommandType::Exit => break,
             util::CommandType::Unknown => {
--- src/util.rs
+++ src/util.rs
@@ -11,40 +11,42 @@ pub struct Command {
     pub cmd_str: String,
 }
 
-pub fn process_input<R: BufRead>(mut reader: R) -> io::Result<String> {
+impl Command {
+    pub fn new<R: BufRead>(reader: R) -> anyhow::Result<Command> {
+        let cmd = match process_input(reader) {
+            Ok(cmd_line) => match Self::find(&cmd_line) {
+                Some(cmd) => cmd,
+                None => {
+                    return Ok(Command {
+                        type_: CommandType::Unknown,
+                        cmd_str: cmd_line,
+                    });
+                }
+            },
+            Err(_) => unreachable!(),
+        };
+
+        Ok(cmd)
+    }
+
+    pub fn find(name: &str) -> Option<Command> {
+        if name.trim() == "exit" {
+            return Some(Command {
+                type_: CommandType::Exit,
+                cmd_str: name.trim().into(),
+            });
+        }
+
+        // All commands are unknown for now
+        None
+    }
+}
+
+fn process_input<R: BufRead>(mut reader: R) -> io::Result<String> {
     let mut input = String::new();
     reader.read_line(&mut input).unwrap();
 
     Ok(input.trim().into())
-}
-
-pub fn read_command<R: BufRead>(reader: R) -> anyhow::Result<Command> {
-    let cmd = match process_input(reader) {
-        Ok(cmd_line) => match find_command(&cmd_line) {
-            Some(cmd) => cmd,
-            None => {
-                return Ok(Command {
-                    type_: CommandType::Unknown,
-                    cmd_str: cmd_line,
-                });
-            }
-        },
-        Err(_) => unreachable!(),
-    };
-
-    Ok(cmd)
-}
-
-fn find_command(name: &str) -> Option<Command> {
-    if name.trim() == "exit" {
-        return Some(Command {
-            type_: CommandType::Exit,
-            cmd_str: name.trim().into(),
-        });
-    }
-
-    // All commands are unknown for now
-    None
 }
 
 #[cfg(test)]
@@ -54,7 +56,7 @@ mod tests {
     #[test]
     fn should_exit_on_find_exit_cmd() {
         let input = "exit";
-        let cmd = find_command(input);
+        let cmd = Command::find(input);
 
         assert!(cmd.as_ref().is_some());
         assert_eq!(cmd.as_ref().unwrap().cmd_str, input);
@@ -65,7 +67,7 @@ mod tests {
     fn should_read_command() {
         let input = "go";
         let reader = io::Cursor::new(input);
-        let cmd = read_command(reader);
+        let cmd = Command::new(reader);
 
         assert!(&cmd.is_ok());
         assert_eq!(CommandType::Unknown, cmd.as_ref().unwrap().type_);
@@ -75,7 +77,7 @@ mod tests {
     #[test]
     fn should_find_command() {
         let cmd_name = "go";
-        let cmd = find_command(cmd_name);
+        let cmd = Command::find(cmd_name);
 
         // All commands are unknown for now
         assert!(cmd.is_none());
